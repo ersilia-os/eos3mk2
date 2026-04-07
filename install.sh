6c4b5c632823d0a1fe37d4577a9ce0736e95b857
Marina18 <marina@ersilia.io>
2026-04-07 14:29:58 +0200

    model refactor: Handle descriptor computation failures gracefully,

--- install.sh
+++
@@ -1,5 +0,0 @@
-/home/marina/anaconda3/envs/eos3mk2/bin/python -m pip install rdkit==2024.3.5
-/home/marina/anaconda3/envs/eos3mk2/bin/python -m pip install tqdm==4.66.5
-/home/marina/anaconda3/envs/eos3mk2/bin/python -m pip install pandas==2.2.3
-/home/marina/anaconda3/envs/eos3mk2/bin/python -m pip install scikit-learn==1.5.2
-/home/marina/anaconda3/envs/eos3mk2/bin/python -m pip install numpy==2.1.2
